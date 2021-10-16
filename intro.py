ff4c942c60d81163c3d568084ab3a488e6b13372
humzahmalik07 <89996008+humzahmalik07@users.noreply.github.com>
2021-10-16 00:52:10 +0000

    intro.py added and updated

---
+++ intro.py
@@ -0,0 +1,19 @@
+def introduction():
+  """ Prints out the introduction 
+  of the game """
+  print("""
+  Batman and Green Lantern are on a mission to find an underground enemy base.
+  While they are looking for the base, the enemy is notified of their mission
+  Batman and Green Lantern need to find their way out and escape the temple.
+
+  You can go only four directions to escape:
+  forward, backward, right, left
+  """)  
+
+  print("""
+  Here is a printed map of the temple. You can use this or a hint, provided in the menu, to find your way out of the temple 
+  """)
+
+  file = open("map.txt", "r")
+  print(file.read())
+  file.close()
